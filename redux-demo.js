1dfbaa2421a5fcf9e307895aefd44a5cba828fac
Mohieddine Farid <farid.mohieddine3@gmail.com>
2023-04-04 12:45:53 +0000

    Explanatory comments of redux terms

--- redux-demo.js
+++ redux-demo.js
@@ -1,6 +1,6 @@
 const redux = require("redux");
 
-const counterReducer = (state, action) => {
+const counterReducer = (state = { counter: 0 }, action) => {
   return {
     counter: state.counter + 1,
   };
@@ -13,4 +13,13 @@ const counterSubscriber = () => {
   console.log(latestState);
 };
 
+// console.log(store.getState());
+
 store.subscribe(counterSubscriber);
+// This function will be executed by redux whenever the data in the store changes
+
+store.dispatch({ type: "INCREMENT" });
+// A dispatch is a function that will be executed by redux whenever an action is dispatched
+// An action is a JavaScript Object that has a type property
+
+// Dispatching an action will trigger the reducer function (counterReducer)
