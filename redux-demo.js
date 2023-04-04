98848d79e8c19952f715f6bbd524c02242f25811
Mohieddine Farid <farid.mohieddine3@gmail.com>
2023-04-04 12:49:51 +0000

    Use of dispatch function, action types

--- redux-demo.js
+++ redux-demo.js
@@ -1,9 +1,19 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === "INCREMENT") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "DECREMENT") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
 };
 
 const store = redux.createStore(counterReducer);
@@ -19,6 +29,7 @@ store.subscribe(counterSubscriber);
 // This function will be executed by redux whenever the data in the store changes
 
 store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
 // A dispatch is a function that will be executed by redux whenever an action is dispatched
 // An action is a JavaScript Object that has a type property
 
